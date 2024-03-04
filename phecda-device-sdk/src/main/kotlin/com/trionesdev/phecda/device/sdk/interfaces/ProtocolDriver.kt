69a83edb9d6d6c5de8cd47aadfe4b2e53c1b574f
Bane.Shi <fengxiaotx@163.com>
2024-03-04 15:51:31 +0800

    feat: init

--- phecda-device-sdk/src/main/kotlin/com/trionesdev/phecda/device/sdk/interfaces/ProtocolDriver.kt
+++ phecda-device-sdk/src/main/kotlin/com/trionesdev/phecda/device/sdk/interfaces/ProtocolDriver.kt
@@ -1,6 +1,40 @@
 package com.trionesdev.phecda.device.sdk.interfaces
 
+import com.trionesdev.phecda.device.sdk.model.CommandRequest
+import com.trionesdev.phecda.device.sdk.model.CommandValue
+
 interface ProtocolDriver {
     fun initialize(sdk: DeviceDriverServiceSDK)
     fun start()
+    fun stop(force: Boolean)
+    fun handleReadCommands(
+        deviceName: String,
+        protocols: MutableMap<String, MutableMap<String, Any>>,
+        reqs: MutableList<CommandRequest>
+    ): MutableList<CommandValue>
+
+    fun handleWriteCommands(
+        deviceName: String,
+        protocols: MutableMap<String, MutableMap<String, Any>>,
+        reqs: MutableList<CommandRequest>,
+        params: MutableList<CommandValue>
+    )
+
+    fun addDevice(
+        deviceName: String,
+        protocols: MutableMap<String, MutableMap<String, Any>>,
+        adminState: String
+    )
+
+    fun updateDevice(
+        deviceName: String,
+        protocols: MutableMap<String, MutableMap<String, Any>>,
+        adminState: String
+    )
+
+    fun removeDevice(
+        deviceName: String,
+        protocols: MutableMap<String, MutableMap<String, Any>>
+    )
+
 }
