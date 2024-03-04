f8f9a696ea5bc1e8cec24c4f9128556a8f2e4a81
Bane.Shi <fengxiaotx@163.com>
2024-03-04 15:51:31 +0800

    feat: init

--- examples/spring-boot/src/main/java/com/trionesdev/phecda/device/VirtualDriver.java
+++ examples/spring-boot/src/main/java/com/trionesdev/phecda/device/VirtualDriver.java
@@ -2,9 +2,14 @@ package com.trionesdev.phecda.device;
 
 import com.trionesdev.phecda.device.sdk.interfaces.DeviceDriverServiceSDK;
 import com.trionesdev.phecda.device.sdk.interfaces.ProtocolDriver;
+import com.trionesdev.phecda.device.sdk.model.CommandRequest;
+import com.trionesdev.phecda.device.sdk.model.CommandValue;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -16,6 +21,37 @@ public class VirtualDriver implements ProtocolDriver {
 
     @Override
     public void start() {
+
+    }
+
+    @Override
+    public void stop(boolean force) {
+
+    }
+
+    @NotNull
+    @Override
+    public List<CommandValue> handleReadCommands(@NotNull String deviceName, @NotNull Map<String, Map<String, Object>> protocols, @NotNull List<CommandRequest> reqs) {
+        return null;
+    }
+
+    @Override
+    public void handleWriteCommands(@NotNull String deviceName, @NotNull Map<String, Map<String, Object>> protocols, @NotNull List<CommandRequest> reqs, @NotNull List<CommandValue> params) {
+
+    }
+
+    @Override
+    public void addDevice(@NotNull String deviceName, @NotNull Map<String, Map<String, Object>> protocols, @NotNull String adminState) {
+
+    }
+
+    @Override
+    public void updateDevice(@NotNull String deviceName, @NotNull Map<String, Map<String, Object>> protocols, @NotNull String adminState) {
+
+    }
+
+    @Override
+    public void removeDevice(@NotNull String deviceName, @NotNull Map<String, Map<String, Object>> protocols) {
 
     }
 }
---
+++ phecda-device-sdk/src/main/kotlin/com/trionesdev/phecda/device/sdk/model/CommandRequest.kt
@@ -0,0 +1,7 @@
+package com.trionesdev.phecda.device.sdk.model
+
+class CommandRequest {
+    val deviceResourceName: String? = null
+    val attributes: MutableMap<String, Any>? = null
+    val type: String? = null
+}
