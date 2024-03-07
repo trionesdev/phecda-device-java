fd1f48f49b545aea9d00a3e8a662882ff8b7a3a4
Bane.Shi <fengxiaotx@163.com>
2024-03-07 11:55:08 +0800

    feat: init

--- phecda-device-sdk/src/main/kotlin/com/trionesdev/phecda/device/sdk/messaging/mqtt/MqttCallback.kt
+++ phecda-device-sdk/src/main/kotlin/com/trionesdev/phecda/device/sdk/messaging/mqtt/MqttCallback.kt
@@ -1,14 +1,18 @@
 package com.trionesdev.phecda.device.sdk.messaging.mqtt
 
+import com.trionesdev.kotlin.log.Slf4j
+import com.trionesdev.kotlin.log.Slf4j.Companion.log
 import com.trionesdev.phecda.device.sdk.interfaces.ProtocolDriver
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
 import org.eclipse.paho.client.mqttv3.MqttMessage
 
+@Slf4j
 class MqttCallback(mqttClient: MqttMessagingClient) : MqttCallbackExtended {
     var mqttClient: MqttMessagingClient? = mqttClient
 
     override fun connectionLost(cause: Throwable?) {
+        log.error("MQTT connection lost: {}", cause?.message)
         mqttClient?.dic?.getInstance(ProtocolDriver::class.java)?.let { protocolDriver ->
             protocolDriver.messagingConnectionLost(cause)
         }
@@ -23,6 +27,6 @@ class MqttCallback(mqttClient: MqttMessagingClient) : MqttCallbackExtended {
     }
 
     override fun connectComplete(reconnect: Boolean, serverURI: String?) {
-
+        mqttClient?.subscribeDefault()
     }
 }
