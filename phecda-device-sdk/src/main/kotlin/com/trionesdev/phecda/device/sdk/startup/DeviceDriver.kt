dda0d215a9dc29899b86d1b85c6ff79e53a6a847
Bane.Shi <fengxiaotx@163.com>
2024-07-24 11:48:13 +0800

    feat: init

--- phecda-device-sdk/src/main/kotlin/com/trionesdev/phecda/device/sdk/startup/DeviceDriver.kt
+++ phecda-device-sdk/src/main/kotlin/com/trionesdev/phecda/device/sdk/startup/DeviceDriver.kt
@@ -1,16 +1,21 @@
 package com.trionesdev.phecda.device.sdk.startup
 
+import com.trionesdev.kotlin.log.Slf4j
+import com.trionesdev.kotlin.log.Slf4j.Companion.log
 import com.trionesdev.phecda.device.bootstrap.environement.Variables
 import com.trionesdev.phecda.device.sdk.interfaces.ProtocolDriver
 import com.trionesdev.phecda.device.sdk.service.DeviceServiceSdkImpl
 
+@Slf4j
 class DeviceDriver {
     companion object {
         @JvmStatic
         fun bootstrap(args: Array<String>, serviceKey: String, serviceVersion: String, driver: ProtocolDriver) {
+            log.info("Phecda Device Driver Starting......")
             Variables.args = args
             val deviceService = DeviceServiceSdkImpl.newDeviceService(serviceKey, serviceVersion, driver)
             deviceService.run()
+            log.info("Phecda Device Driver Started")
         }
     }
 }
